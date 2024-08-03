94772fe34c77ff95e475312f0dddd64ccf4d8427
abdulmossawer <mossawer786@gmail.com>
2024-08-03 21:24:06 +0530

    Navbar and Hero Section Completed

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Link from "next/link";
+import { Button } from "@/components/ui/moving-border";
+import { Spotlight } from "@/components/ui/Spotlight";
+
+const HeroSection = () => {
+  return (
+    <div className=" h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
+        <Spotlight
+        className="-top-40 left-0 md:left-60 md:-top-20"
+        fill="white"
+      />
+      <div className="mt-20 p-4 relative z-10 w-full text-center">
+        <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400"
+        >
+        Unleash Your Potential. Step with Adidas.
+        </h1>
+        <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
+        Step into the world of Adidas and transform your athletic journey today. Whether you're just starting out or seeking to enhance your performance, join us to unlock your full potential and achieve greatness.
+        </p>
+        <div className="mt-4">
+        <Link href={'/explore'}>
+        <Button
+        borderRadius="1.75rem"
+        className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
+        >
+            Explore
+        </Button>
+        </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,41 @@
+"use client";
+import Link from "next/link";
+import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+import { cn } from "@/utils/utils";
+
+function Navbar({ className }: { className?: string }) {
+  const [active, setActive] = useState<string | null>(null);
+  return (
+    <div
+      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+    >
+      <Menu setActive={setActive}>
+        <Link href={"/"}>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Home"
+          ></MenuItem>
+        </Link>
+        <MenuItem setActive={setActive} active={active} item="Our Catogeries">
+          <div className="flex flex-col space-y-4 text-sm">
+            <HoveredLink href="/shoes">Sneaker</HoveredLink>
+            <HoveredLink href="/shoes">Sports</HoveredLink>
+            <HoveredLink href="/shoes">Casual</HoveredLink>
+            <HoveredLink href="/shoes">Gym</HoveredLink>
+          </div>
+        </MenuItem>
+        <Link href={"/contact"}>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Contact Us"
+          ></MenuItem>
+        </Link>
+      </Menu>
+    </div>
+  );
+}
+
+export default Navbar;
